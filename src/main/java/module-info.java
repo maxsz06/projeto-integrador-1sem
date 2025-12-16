652b04e6564ec9a54ce858f20a8a0cdf4ad95a12
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2025-12-16 08:33:58 -0300

    Estrutura de Pastas Criada

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module sp.jandira.senai.maxwillian.projetointegrador1sem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens sp.jandira.senai.maxwillian.projetointegrador1sem to javafx.fxml;
+    exports sp.jandira.senai.maxwillian.projetointegrador1sem;
+    exports sp.jandira.senai.maxwillian.projetointegrador1sem.ui;
+    opens sp.jandira.senai.maxwillian.projetointegrador1sem.ui to javafx.fxml;
+}
---
+++ src/main/java/sp/jandira/senai/maxwillian/projetointegrador1sem/Launcher.java
@@ -0,0 +1,10 @@
+package sp.jandira.senai.maxwillian.projetointegrador1sem;
+
+import javafx.application.Application;
+import sp.jandira.senai.maxwillian.projetointegrador1sem.ui.TelaPrincipal;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(TelaPrincipal.class, args);
+    }
+}
